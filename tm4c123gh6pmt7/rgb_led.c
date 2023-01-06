e64454f533de45664b1c5807afb4b442d553a3fc
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-01-07 01:09:37 +0530

    Update rgb_led.c

--- tm4c123gh6pmt7/rgb_led.c
+++ tm4c123gh6pmt7/rgb_led.c
@@ -3,33 +3,11 @@
  *  File: main.c // this is the main file for the project
  */
 #include <stdint.h>
-/*Register mapping*/
+#include "TM4C123.h"
 
-//(*((volatile uint32_t *)0x40000000))
-
-// bus control registers
-#define GPIOHBCTL_Base (*((volatile uint32_t *)0x400FE000))
-#define GPIOHBCTL_Reg (*((volatile uint32_t *)0x400FE06C))
-
-// clock control registers
-#define RCGCGPIO_Base (*((volatile uint32_t *)0x400FE000))
 #define RCGCGPIO_Reg (*((volatile uint32_t *)0x400FE608))
-
-// GPIO control registers
-#define GPIO_PORT_F_AHB_Base (*((volatile uint32_t *)0x4005D000))
-#define GPIO_PORT_F_DATA (*((volatile uint32_t *)0x4005D000))
-#define GPIODIR_PORTF (*((volatile uint32_t *)0x4005D400))
-#define GPIOAFSEL_PORTF (*((volatile uint32_t *)0x4005D420))
-#define GPIODR2R (*((volatile uint32_t *)0x4005D500))
-#define GPIODR4R (*((volatile uint32_t *)0x4005D504))
-#define GPIODR8R (*((volatile uint32_t *)0x4005D508))
-#define GPIODEN (*((volatile uint32_t *)0x4005D51C))
-
-/*Bit mapping*/
-#define PORTF_AHB_SEL_bit 5
-#define R5 5    // provide clock to portF
-
-/* application specific macros*/
+	
+#define clk_portf 5    // provide clock to portF
 #define red_led 1
 #define blue_led 2
 #define green_led 3
@@ -40,7 +18,7 @@
 #define GPIO 0
 
 /*function declarations*/
-void sysInit(void);
+
 void clockInit(void);
 void gpioPortInit(void);
 void gpioPinInit(unsigned char, unsigned char, unsigned char);
@@ -48,23 +26,23 @@ void led(unsigned char, unsigned char);
 
 int main(void)
 {
-    sysInit();
-    clockInit();
-    gpioPinInit(GPIO, OUTPUT, red_led);
-    gpioPinInit(GPIO, OUTPUT, blue_led);
-    gpioPinInit(GPIO, OUTPUT, green_led);
-    led(red_led, ON);
+	clockInit();
+	gpioPinInit(GPIO, OUTPUT, red_led);
+	gpioPinInit(GPIO, OUTPUT, blue_led);
+	gpioPinInit(GPIO, OUTPUT, green_led);
+	led(red_led, ON);
+	led(blue_led, ON);
+	led(green_led, ON);
+	// white colour is displayed
 	return 0;
-}
-
-void sysInit(void)
-{
-    GPIOHBCTL_Reg |= (1 << PORTF_AHB_SEL_bit);  // AHB for PORTF
 }
 
 void clockInit(void)
 {
-    RCGCGPIO_Reg |= (1 << R5);  // clock for PORTF
+	// board is set in such a way that we don't have to make any change in system RCC register
+	// RCC default value is 0x078E.3AD1
+   RCGCGPIO_Reg |= (1 << clk_portf);  // clock for PORTF
+	 // SYSCTL->RCGC0 |= (1 << clk_portf);	// legacy register not used here.
 }
 
 void gpioPortInit()
@@ -76,50 +54,49 @@ void gpioPinInit(unsigned char function, unsigned char dir, unsigned char pin)
 {
     if (function == GPIO)
     {
-        GPIOAFSEL_PORTF &= ~(1 << pin);
+			GPIOF->AFSEL &= ~(1 << pin);
     }
 
-    GPIODR2R |= ((1 << red_led) | (1 << blue_led) | (1 << green_led));
-
+    GPIOF->DR2R |= ((1 << red_led) | (1 << blue_led) | (1 << green_led));
+		
     if (dir)
     {
-        GPIODIR_PORTF |= (1 << pin);
+        GPIOF->DIR |= (1 << pin);
     }
     else
     {
-        GPIODIR_PORTF &= ~(1 << pin);
+        GPIOF->DIR &= ~(1 << pin);
     }
-
-    GPIODEN |= ((1 << red_led) | (1 << blue_led) | (1 << green_led));
+    GPIOF->DEN |= ((1 << red_led) | (1 << blue_led) | (1 << green_led));
 }
 
 void led(unsigned char colour, unsigned char status)
 {
     if (status && (colour == red_led))
     {
-        GPIO_PORT_F_DATA |= (1 << red_led);
-    }
+        GPIOF->DATA |= (1 << red_led);
+		}
     else if (status && (colour == blue_led))
     {
-        GPIO_PORT_F_DATA |= (1 << blue_led);
+        GPIOF->DATA |= (1 << blue_led);
     }
     else if (status && (colour == green_led))
     {
-        GPIO_PORT_F_DATA |= (1 << green_led);
+        GPIOF->DATA |= (1 << green_led);
     }
     else
     {
         if (colour == red_led)
         {
-            GPIO_PORT_F_DATA &= ~(1 << red_led);
+            GPIOF->DATA &= ~(1 << red_led);
         }
         if (colour == blue_led)
         {
-            GPIO_PORT_F_DATA &= ~(1 << blue_led);
+            GPIOF->DATA &= ~(1 << blue_led);
         }
         if (colour == green_led)
         {
-            GPIO_PORT_F_DATA &= ~(1 << green_led);
+            GPIOF->DATA &= ~(1 << green_led);
         }
     }
 }
