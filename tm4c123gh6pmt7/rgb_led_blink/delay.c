7ed18d4d7356d023333284a75c8663af63f898b6
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-01-27 03:37:24 +0530

    added delayUS

--- tm4c123gh6pmt7/rgb_led_blink/delay.c
+++ tm4c123gh6pmt7/rgb_led_blink/delay.c
@@ -1,12 +1,13 @@
 #include "delay.h"
-
 /*
  * @param unsigned char wait
  * @brief provides a blocking delay for "wait" miliseconds
 */
-void delayMS(uint16_t wait)
+
+void delayMS(uint32_t wait1)
 {
-	while (wait != 0)
+	wait1 = wait1 * 16;
+	while (wait1 != 0)
 	{
 		/*provide clock to 16/32 bit timers*/
 		RCGCTIMER_Reg |= (1 << TIMER0_bit);
@@ -25,7 +26,7 @@ void delayMS(uint16_t wait)
 		TIMER0->TBMR |= (1 << one_shot_mode_bit);
 		
 		/*sets the upper bound for the timeout event*/
-		TIMER0->TBILR = 0x00000400;	/*0.1 mili second time out*/
+		TIMER0->TBILR = 0x00000400;	/*64 micro second time out*/
 		
 		/*enable the gptm block 0 timer B*/
 		TIMER0->CTL |= (1 << TBEN_bit);
@@ -36,6 +37,46 @@ void delayMS(uint16_t wait)
 		/*resetting the flag*/
 		TIMER0->ICR |= (1 << TBTORIS_bit);
 		
-		wait -= 1;
+		wait1 -= 1;
+	}
+}
+
+/*
+ * @param unsigned char wait
+ * @brief provides a blocking delay for "wait" microseconds
+*/
+void delayUS(uint32_t wait1)
+{
+	while (wait1 != 0)
+	{
+		/*provide clock to 16/32 bit timers*/
+		RCGCTIMER_Reg |= (1 << TIMER0_bit);
+		
+		/*provide clock to 32/64 bit timers*/
+		/*uncomment the following line to provide clock to 32/64 bit timers in case of large delays*/
+		/*RCGCWTIMER_Reg |= (1 << TIMER0_bit);*/
+		
+		/*disable the gptm block 0 timer B*/
+		TIMER0->CTL &= ~(1 << TBEN_bit);
+		
+		/*For a 16/32-bit timer, this value selects the 16-bit timer configuration*/
+		TIMER0->CFG = 0x00000004;
+		
+		/*one short mode*/
+		TIMER0->TBMR |= (1 << one_shot_mode_bit);
+		
+		/*sets the upper bound for the timeout event*/
+		TIMER0->TBILR = 0x00000010;	/*1 micro second time out*/
+		
+		/*enable the gptm block 0 timer B*/
+		TIMER0->CTL |= (1 << TBEN_bit);
+		
+		/*polling for timeout*/
+		while (!(TIMER0->RIS & (1 << TBTORIS_bit)));
+		
+		/*resetting the flag*/
+		TIMER0->ICR |= (1 << TBTORIS_bit);
+		
+		wait1 -= 1;
 	}
 }
