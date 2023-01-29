7894b5d4a542eee042adf653302c7a604273c4d1
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-01-29 19:45:00 +0530

    added configuration.h

---
+++ tm4c123gh6pmt7/uart/examples/HC-05/configuration.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIGURATION_H_
+#define _CONFIGURATION_H_
+
+#include <TM4C123.h>
+#include <stdint.h>
+
+#ifdef ONBOARD_LEDS_H
+#include "onBoard_LEDs.h"
+#endif
+
+#ifdef UART_H
+#include "uart.h"
+#endif
+
+#endif
