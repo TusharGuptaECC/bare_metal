8222d3ce33b673430e26a08e6518bb4df0408029
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-02-10 09:52:27 +0530

    control on board led via bluetooth

--- stm32_NUCLEO_F401RE/usart/examples/HC-05/main.c
+++ stm32_NUCLEO_F401RE/usart/examples/HC-05/main.c
@@ -1,1 +1,33 @@
-main
+/*
+  Author: Tushar Gupta
+Software: Keil uVision
+*/
+
+#include <stm32f401xe.h>
+#include "SonalKunj_delay.h"
+#include "SonalKunj_onBoards.h"
+#include "SonalKunj_usart.h"
+
+int main(void)
+{
+	unsigned char cmd;
+	
+	initOnBoardLED();
+	usart_1_Init();
+	usart_1_Print("Hello world\n");
+	usart_1_Print("BCR ready to take orders \n");
+	while (1)
+	{
+		cmd = usart_1_Receive();
+		if (cmd == 'a')
+		{
+			led(ON);
+		}
+		if (cmd == 'b')
+		{
+			led(OFF);
+		}
+	}
+	return 0;
+}
+
