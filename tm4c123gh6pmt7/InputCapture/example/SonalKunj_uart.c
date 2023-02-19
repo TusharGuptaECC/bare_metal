f6305008fcdbd5372fe309f29a47a3da12481643
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-02-19 09:32:02 +0530

    dht-11
    This code uploads the readings of dht-11 to nextion display.

---
+++ tm4c123gh6pmt7/InputCapture/example/SonalKunj_uart.c
@@ -0,0 +1,606 @@
+#include "SonalKunj_uart.h"
+
+/* all uart init functions */
+void uart_7_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_7);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOE->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOE->CR |= ((1 << UART_7_RX) | (1 << UART_7_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOE->AFSEL |= ((1 << UART_7_RX) | (1 << UART_7_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOE->PCTL |= ((1 << 4) | (1 << 0));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOE->DEN |= ((1 << UART_7_RX) | (1 << UART_7_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOE->DR8R |= ((1 << UART_7_RX) | (1 << UART_7_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOE->SLR |= ((1 << UART_7_RX) | (1 << UART_7_TX));
+	
+	/* disable the UART */
+	UART7->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART7->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART7->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART7->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART7->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_6_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_6);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOD->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOD->CR |= ((1 << UART_6_RX) | (1 << UART_6_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOD->AFSEL |= ((1 << UART_6_RX) | (1 << UART_6_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOD->PCTL |= ((1 << 20) | (1 << 16));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOD->DEN |= ((1 << UART_6_RX) | (1 << UART_6_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOD->DR8R |= ((1 << UART_6_RX) | (1 << UART_6_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOD->SLR |= ((1 << UART_6_RX) | (1 << UART_6_TX));
+	
+	/* disable the UART */
+	UART6->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART6->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART6->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART6->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART6->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_5_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_5);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOE->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOE->CR |= ((1 << UART_5_RX) | (1 << UART_5_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOE->AFSEL |= ((1 << UART_5_RX) | (1 << UART_5_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOE->PCTL |= ((1 << 20) | (1 << 16));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOE->DEN |= ((1 << UART_5_RX) | (1 << UART_5_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOE->DR8R |= ((1 << UART_5_RX) | (1 << UART_5_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOE->SLR |= ((1 << UART_5_RX) | (1 << UART_5_TX));
+	
+	/* disable the UART */
+	UART5->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART5->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART5->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART5->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART5->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_4_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_4);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOC->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOC->CR |= ((1 << UART_4_RX) | (1 << UART_4_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOC->AFSEL |= ((1 << UART_4_RX) | (1 << UART_4_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOC->PCTL |= ((1 << 20) | (1 << 16));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOC->DEN |= ((1 << UART_4_RX) | (1 << UART_4_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOC->DR8R |= ((1 << UART_4_RX) | (1 << UART_4_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOC->SLR |= ((1 << UART_4_RX) | (1 << UART_4_TX));
+	
+	/* disable the UART */
+	UART4->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART4->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART4->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART4->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART4->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_3_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_3);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOC->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOC->CR |= ((1 << UART_3_RX) | (1 << UART_3_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOC->AFSEL |= ((1 << UART_3_RX) | (1 << UART_3_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOC->PCTL |= ((1 << 28) | (1 << 24));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOC->DEN |= ((1 << UART_3_RX) | (1 << UART_3_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOC->DR8R |= ((1 << UART_3_RX) | (1 << UART_3_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOC->SLR |= ((1 << UART_3_RX) | (1 << UART_3_TX));
+	
+	/* disable the UART */
+	UART3->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART3->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART3->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART3->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART3->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_2_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_2);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOD->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOD->CR |= ((1 << UART_2_RX) | (1 << UART_2_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOD->AFSEL |= ((1 << UART_2_RX) | (1 << UART_2_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOD->PCTL |= ((1 << 28) | (1 << 24));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOD->DEN |= ((1 << UART_2_RX) | (1 << UART_2_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOD->DR8R |= ((1 << UART_2_RX) | (1 << UART_2_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOD->SLR |= ((1 << UART_2_RX) | (1 << UART_2_TX));
+	
+	/* disable the UART */
+	UART2->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART2->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART2->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART2->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART2->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_1_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_1);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOB->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOB->CR |= ((1 << UART_1_RX) | (1 << UART_1_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOB->AFSEL |= ((1 << UART_1_RX) | (1 << UART_1_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOB->PCTL |= ((1 << 4) | (1 << 0));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOB->DEN |= ((1 << UART_1_RX) | (1 << UART_1_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOB->DR8R |= ((1 << UART_1_RX) | (1 << UART_1_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOB->SLR |= ((1 << UART_1_RX) | (1 << UART_1_TX));
+	
+	/* disable the UART */
+	UART1->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART1->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART1->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART1->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART1->CTL |= (1 << UART_EN_bit);
+	
+}
+
+void uart_0_Init(void)
+{
+	/* provide clock to uart 7 */
+	RCGCUART_Reg |= (1 << UART_0);
+	
+	/* clock to port e has been already provided by clockInit */
+	
+	/* unlock the gpio commit register */
+	GPIOA->LOCK = 0x4C4F434B;
+	
+	/* commit register unmask the required bits to unlock */
+	GPIOA->CR |= ((1 << UART_0_RX) | (1 << UART_0_TX));
+	
+	/* enable the alternate function control on given pins*/
+	GPIOA->AFSEL |= ((1 << UART_0_RX) | (1 << UART_0_TX));
+
+	/* selecting uart function for PE0 and PE1*/
+	GPIOA->PCTL |= ((1 << 4) | (1 << 0));
+	
+	/* enable the digital operation on PE0 amd PE1 */
+	GPIOA->DEN |= ((1 << UART_0_RX) | (1 << UART_0_TX));
+	
+	/* select the drive strength 8mA for slew rate control capability */
+	GPIOA->DR8R |= ((1 << UART_0_RX) | (1 << UART_0_TX));
+	
+	/* slew rate control enable for RX and TX*/
+	GPIOA->SLR |= ((1 << UART_0_RX) | (1 << UART_0_TX));
+	
+	/* disable the UART */
+	UART0->CTL &= ~(1 << UART_EN_bit);
+	
+	/* BRD = (16,000,000/(16 x 9600)) = 104.16*/
+	/* UARTIBRD = 104 i.e 0x68 */
+	/* UARTFBRD[DIVFRAC] = integer(BRDF * 64 + 0.5) formulae for fraction register */
+	/* UARTFBRD = integer(0.16 x 64 + 0.5) = 10.74 i.e 11 i.e 0xB */
+	/* did all calculation manually to avoid cpu load*/
+	
+	/* integer BRD register */
+	UART0->IBRD = 0x68;	/* 16 bits are used out of 32 bits */
+	
+	/* fraction BRD register */
+	UART0->FBRD = 0xB;	/* 6 bits are used out of 32 bits */
+	
+	/* parity disabled, stick parity disabled, FIFO mode disabled */
+	/* only 8 bit data frame is selected with one stop bit */
+	UART0->LCRH = 0x00000060;
+	
+	/* URATCC default */
+	//UART7->CC = 0x0;
+	
+	/* enable the UART7 */
+	UART0->CTL |= (1 << UART_EN_bit);
+	
+}
+
+/* all transmit functions */
+void uart_7_Transmit(unsigned char data)
+{
+	while(UART7->FR & (1 << UART_busy_transmit_bit));
+	UART7->DR = data;				
+}
+
+void uart_6_Transmit(unsigned char data)
+{
+	while(UART6->FR & (1 << UART_busy_transmit_bit));
+	UART6->DR = data;				
+}
+
+void uart_5_Transmit(unsigned char data)
+{
+	while(UART5->FR & (1 << UART_busy_transmit_bit));
+	UART5->DR = data;				
+}
+
+void uart_4_Transmit(unsigned char data)
+{
+	while(UART4->FR & (1 << UART_busy_transmit_bit));
+	UART4->DR = data;				
+}
+
+void uart_3_Transmit(unsigned char data)
+{
+	while(UART3->FR & (1 << UART_busy_transmit_bit));
+	UART3->DR = data;				
+}
+
+void uart_2_Transmit(unsigned char data)
+{
+	while(UART2->FR & (1 << UART_busy_transmit_bit));
+	UART2->DR = data;				
+}
+
+void uart_1_Transmit(unsigned char data)
+{
+	while(UART1->FR & (1 << UART_busy_transmit_bit));
+	UART1->DR = data;				
+}
+
+void uart_0_Transmit(unsigned char data)
+{
+	while(UART0->FR & (1 << UART_busy_transmit_bit));
+	UART0->DR = data;				
+}
+
+/* all uart print functions */
+void uart_7_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_7_Transmit(*(message++));
+	}
+}
+
+void uart_6_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_6_Transmit(*(message++));
+	}
+}
+
+void uart_5_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_5_Transmit(*(message++));
+	}
+}
+
+void uart_4_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_4_Transmit(*(message++));
+	}
+}
+
+void uart_3_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_3_Transmit(*(message++));
+	}
+}
+
+void uart_2_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_2_Transmit(*(message++));
+	}
+}
+
+void uart_1_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_1_Transmit(*(message++));
+	}
+}
+
+void uart_0_Print(unsigned char *message)
+{
+  while(*message)
+	{
+		uart_0_Transmit(*(message++));
+	}
+}
+
+/* all receive functions */
+unsigned char uart_7_Receive(void)
+{
+	while (!(UART7->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART7->DR;
+}
+
+unsigned char uart_6_Receive(void)
+{
+	while (!(UART6->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART6->DR;
+}
+
+unsigned char uart_5_Receive(void)
+{
+	while (!(UART5->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART5->DR;
+}
+
+unsigned char uart_4_Receive(void)
+{
+	while (!(UART4->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART4->DR;
+}
+
+unsigned char uart_3_Receive(void)
+{
+	while (!(UART3->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART3->DR;
+}
+
+unsigned char uart_2_Receive(void)
+{
+	while (!(UART2->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART2->DR;
+}
+
+unsigned char uart_1_Receive(void)
+{
+	while (!(UART1->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART1->DR;
+}
+
+unsigned char uart_0_Receive(void)
+{
+	while (!(UART0->FR & (1 << UART_RXFF_bit)));
+	return (unsigned char)UART0->DR;
+}
+
--- tm4c123gh6pmt7/InputCapture/example/main.c
+++ tm4c123gh6pmt7/InputCapture/example/main.c
@@ -1,1 +1,190 @@
+#define ONBOARD_LEDS_H
+#define NEXTION_H
+#define DELAY_H
+#define SWITCH_H
 
+#include "configuration.h"
+
+unsigned char settings[3] = {16,50,3};
+
+#define PIOSC 0	/*Precision internal oscillator*/
+#define MOSC 1	/*External main oscillator*/
+#define clk_portb 1
+#define dht_pin 2	// portf
+
+volatile uint32_t present_time = 0;
+volatile uint32_t past_time = 0;
+uint32_t dht_data;
+volatile int dht_count = 0;
+	
+void TIMER1A_Handler(void)
+{
+	//led(red_led, ON);
+	if (TIMER1->MIS & (1 << 2))
+	{
+		TIMER1->ICR |= (1 << 2);
+		past_time = present_time;
+		present_time = ((TIMER1->TAR & 0x0000FFFF));
+		dht_count = dht_count + 1;
+	}
+}
+
+void clockInit(unsigned char clk_src);
+void dhtInit(void);
+
+void dhtReadData(void);
+
+int main(void)
+{
+	unsigned char response;
+	clockInit(MOSC);
+	//gpioPinInit(GPIO, OUTPUT, blue_led);
+	gpioPinInit(GPIO, OUTPUT, green_led);
+	gpioPinInit(GPIO, OUTPUT, red_led);
+	nextionInit();
+	
+	while (1)
+	{
+//		for data transmission in form of text.
+		//delayMS(2000);
+		if (nextionRead_1() == 2)
+		{
+			dhtInit();
+		  dhtReadData();
+			nextionSendSensorData(((dht_data >> 24) & 0x000000FF), ((dht_data >> 8) & 0x000000FF),3);
+			led(red_led, ON);
+		}
+		else//if (nextionRead_1() == 0)
+		{
+			led(green_led, OFF);
+		}
+		
+// for manual
+//		response = nextionRead_1();
+//		if (response == 6)
+//		{
+//			led(red_led, ON);
+//		}
+//		if (response == 7)
+//		{
+//			led(red_led, OFF);
+//		}
+//		if (response == 8)
+//		{
+//			led(blue_led, ON);
+//		}
+//		if (response == 9)
+//		{
+//			led(blue_led, OFF);
+//		}
+//		if (response == 10)
+//		{
+//			led(green_led, ON);
+//		}
+//		if (response == 11)
+//		{
+//			led(green_led, OFF);
+//		}
+//		if (response == 12)
+//		{
+//			led(red_led, ON);
+//			led(green_led, ON);
+//		}
+//		if (response == 13)
+//		{
+//			led(red_led, OFF);
+//			led(green_led, OFF);
+//		}
+	// for settings
+//	response = nextionRead_1();
+//	if (response == 4)
+//	{
+//		led(red_led, ON);
+//		while (1)
+//		{
+//			response = nextionRead_2();
+//			if (response == 200)
+//			{
+//				saveSettingsFromNextion();
+//				led(red_led, OFF);
+//				break;
+//			}
+//		}
+//		
+//	}
+//	if (settings[0] == 1)
+//	{
+//		led(red_led, ON);
+//	}
+//	if (settings[1] == 2)
+//	{
+//		led(blue_led, ON);
+//	}
+//	if (settings[2] == 30)
+//	{
+//		led(green_led, ON);
+//	}
+	}
+	return 0;
+}
+
+void clockInit(unsigned char clk_src)
+{
+	// RCC default value is 0x078E.3AD1
+	uint32_t i;
+	if (clk_src == MOSC)
+	{
+		SYSCTL->RCC = 0x000E3D40;
+	}
+	if (clk_src == PIOSC)
+	{
+		SYSCTL->RCC = 0x078E3AD1;
+	}
+  for (i = 0; i < 10000; i++);
+	RCGCGPIO_Reg |= ((1 << clk_portf) | (1 << clk_portb));  // clock for PORTF
+	// SYSCTL->RCGC0 |= (1 << clk_portf);	// legacy register not used here.
+}
+
+void dhtInit(void)
+{
+	gpioPinInit(GPIO, OUTPUT, dht_pin);
+	led(dht_pin, ON);
+	delayMS(1000);
+	led(dht_pin, OFF);
+	delayMS(25);
+	led(dht_pin, ON);
+	delayUS(5);
+	led(dht_pin, OFF);
+	gpioPinInit(GPIO, INPUT, dht_pin);
+	inputCaptureInit(dht_pin);
+}
+
+void dhtReadData(void)
+{
+	while(dht_count < 31)
+	{
+		if(dht_count < 3)
+		{
+			//led(green_led, ON);
+			// do nothing
+			{}	// avoid compiler optimization
+		}
+		else if (present_time - past_time > 1600)//past_time - present_time > 1600 ) // 100us
+		{
+			dht_data |= (1 << (31 - dht_count + 3));
+		}
+		else
+		{
+			dht_data &= ~(1 << (31 - dht_count + 3));
+		}
+		
+	}
+	while(dht_count < 42)
+	{
+		{}// do nothing
+		
+	}
+	TIMER1->CTL &= ~(1 << TAEN_bit); // off the timer
+	// reset the timer
+	
+}
