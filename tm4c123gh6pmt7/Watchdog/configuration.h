361f4fe546a8716859de848748e3d94b6e4c3fcf
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-02-25 14:47:32 +0530

    commit 1

---
+++ tm4c123gh6pmt7/Watchdog/configuration.h
@@ -0,0 +1,39 @@
+#ifndef _CONFIGURATION_H_
+#define _CONFIGURATION_H_
+
+#include <TM4C123.h>
+#include <stdint.h>
+
+#ifdef ONBOARD_LEDS_H
+#include "SonalKunj_onBoard_LEDs.h"
+#endif
+
+#ifdef UART_H
+#include "SonalKunj_uart.h"
+#endif
+
+#ifdef DELAY_H
+#include "SonalKunj_delay.h"
+#endif
+
+#ifdef NEXTION_H
+#include "SonalKunj_nextion.h"
+#endif
+
+#ifdef ONBOARD_SWITCH_H
+#include "SonalKunj_onBoard_switches.h"
+#endif
+
+#ifdef ADC_H
+#include "SonalKunj_adc.h"
+#endif
+
+#ifdef PWM_H
+#include "SonalKunj_pwm.h"
+#endif
+
+#ifdef WATCHDOG_H
+#include "SonalKunj_watchdog.h"
+#endif
+
+#endif
